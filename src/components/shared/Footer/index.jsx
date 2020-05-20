3dfaf8134069e10a98754ea59a07fa52fa3dea32
Tiago Cabral <tiagorc.cabral@gmail.com>
2020-05-20 13:57:04 -0300

    Adds styled divs and footer component

---
+++ src/components/shared/Footer/index.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { FooterContainer, TextContainer, FooterText } from "./styles"
+
+function Footer() {
+  return (
+    <FooterContainer>
+      <TextContainer>
+        <FooterText>voltar ao topo</FooterText>
+      </TextContainer>
+    </FooterContainer>
+  )
+}
+
+export default Footer
