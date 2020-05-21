d6d68ae524b5d06b9a3dc3a5213462452535bce1
Tiago Cabral <tiagorc.cabral@gmail.com>
2020-05-21 14:59:16 -0300

    Install and inital config of redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import React from "react";
-import { Router } from "react-router-dom";
+import {Router} from "react-router-dom";
+import {Provider} from 'react-redux';
 import history from "./services/history";
 import Routes from "./routes";
 import "./utils/fontawesome";
+import store from "./services/store";
 
 function App() {
     return (
-        <Router history={history}>
-            <Routes />
-        </Router>
+        <Provider store={store}>
+            <Router history={history}>
+                <Routes/>
+            </Router>
+        </Provider>
+
     );
 }
 
