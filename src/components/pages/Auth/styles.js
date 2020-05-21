fa637ee52fe3b1cb1edfe775540474dc121c9499
Tiago Cabral <tiagorc.cabral@gmail.com>
2020-05-21 15:42:37 -0300

    Improving a bit the login page style

---
+++ src/components/pages/Auth/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const ExternalDivContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const ExternalContainerMargin = styled.div`
+    margin-top: 5em;
+    width: 100%;
+`;
